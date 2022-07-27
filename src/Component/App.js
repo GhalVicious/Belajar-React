e7814e1901fe22d517cbb603713372bef6bcf2ba
Galih <ghaljr24@gmail.com>
2022-07-27 11:43:57 +0700

    first commit

---
+++ src/Component/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Styles/App.css"
+
+const App = () =>  {
+        return(
+        <div>
+            <h1>
+            Galih Nur Ikhsan
+            </h1>
+        </div>
+     )
+}
+
+export default App;
